2aa050ff925e03c467f7ef872293a85a138c0099
rcr4m0s <ramosersei@gmail.com>
2026-09-05 23:19:15 +0800

    Add analytics_1D.py challenge file

---
+++ analytics_1D.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+sales = np.array([1200, 4500, 800, 3200, 6000, 1500, 900, 7500, 2100, 4000, 1100, 5200])
+
+net_sales = sales * 0.88
+peak_sales = sales[sales >= 4000]
+
+average_net_sales = net_sales.mean()
+total_revenue = peak_sales.sum()
+
+print("Net sales: ", net_sales)
+print("Peak sales; ", peak_sales)
+print("Average net sale:", average_net_sales)
+print("Sum of peak sales:", total_revenue)
